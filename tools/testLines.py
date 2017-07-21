93fce7a8073a8359b419684c724f81014fde6b46
Andre <apcianflone@gmail.com>
2017-07-20 21:35:19 -0400

    cleanup

--- tools/testLines.py
+++ tools/testLines.py
@@ -11,21 +11,21 @@ from ft import FASTex
 from vis import draw_keypoints
 
 if __name__ == '__main__':
-    
+
     outputDir = '/tmp'
     edgeThreshold = 13
-    
+
     ft = FASTex(edgeThreshold= edgeThreshold, nlevels=-1, minCompSize = 4)
-    
+
     imgName = '/datagrid/personal/TextSpotter/evaluation-sets/bornDigital/img_100.png'
-        
+
     if len(sys.argv) > 1:
         if sys.argv[1].endswith(".png") or sys.argv[1].endswith(".jpg"):
             imgName = sys.argv[1]
-        
+
     img = cv2.imread(imgName, 0)
     imgc = cv2.imread(imgName)
-    
+
     #print(out)
     segmentations = ft.getCharSegmentations(img, outputDir, 'base')
     print segmentations
@@ -33,13 +33,13 @@ if __name__ == '__main__':
         rectn = segmentations[i, :]
         rectn[2] += rectn[0]
         rectn[3] += rectn[1]
-        
+
     lines = ft.findTextLines(outputDir, 'base')
-        
-    
+
+
     for i in range(lines.shape[0]):
             line = lines[i]
             lineSegm = ft.getNormalizedLine(i)
             cv2.imshow("textLine", lineSegm)
             cv2.waitKey(0)
-        
+
