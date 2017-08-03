55865f784037301a8de495a3c5662526b14b71a8
Andre <apcianflone@gmail.com>
2017-08-03 12:50:27 -0400

    tmp

--- tools/main.py
+++ tools/main.py
@@ -22,10 +22,14 @@ def drawCharBox(img, segmentations, idx):
   cv2.rectangle(img, pt1, pt2, (0, 0, 255), 1)
 
 def drawCharBoxes(img, segmentations, start_idx=0, max_boxes=None):
-  """ Draw all character boxes on image """
+  """
+  Draw all character boxes on image
+  Args:
+    max_boxes - max number of boxes to draw
+  """
   for i in range(start_idx, segmentations.shape[0]):
     drawCharBox(img, segmentations, i)
-    if max_boxes is not None and i >= max_boxes: break;
+    if max_boxes is not None and i >= start_idx + max_boxes: break;
 
 def drawCharMask(img, ft, segmentations, idx):
   """ Draw character mask on img with mask idx corresponding to character
@@ -48,6 +52,10 @@ def drawCharMask(img, ft, segmentations, idx):
   img[y:y+rows, x:x+cols] = dst
 
 def drawCharMasks(img, ft, segmentations, start_idx=0, max_chars=None):
+  """
+  Args:
+    max_boxes - max number of boxes to draw
+  """
   bg_mask = np.full((img.shape[0], img.shape[1]), 255, dtype='uint8')
   fg_mask = np.zeros((img.shape[0], img.shape[1]), dtype='uint8')
   for idx in range(start_idx, segmentations.shape[0]):
@@ -58,7 +66,7 @@ def drawCharMasks(img, ft, segmentations, start_idx=0, max_chars=None):
     y = segmentations[idx][1] # where mask starts in original image
     bg_mask[y:y+rows, x:x+cols] = mask_inv
     fg_mask[y:y+rows, x:x+cols] = mask
-    if max_chars is not None and idx >= max_chars: break;
+    if max_chars is not None and idx >= start_idx + max_chars: break;
 
   # Background from original
   bg = cv2.bitwise_and(img,img,mask=bg_mask)
@@ -70,17 +78,20 @@ def drawCharMasks(img, ft, segmentations, start_idx=0, max_chars=None):
   dst = cv2.add(bg,fg)
   img[:,:,:] = dst
 
-def viz_location(img, ft, segmentations, cumulative=False):
+def viz_location(img_o, ft, segmentations, cumulative=False, max_chars=None):
   '''
   Save char boxes and masks to debug
   Args:
     cumulative - if true each image includes previous masks
   '''
-  max_chars = 100
-  for i in range(0,1001, 100):
-    drawCharMasks(img, ft, segmentations, start_idx=i, max_chars=max_chars+1)
-    drawCharBoxes(img, segmentations, start_idx=i, max_boxes=max_chars+1)
-    name = "{}/box_{}-{}.jpg".format(output_dir, i, i + max_chars)
+  img = img_o
+  step=100
+  for i in range(0,max_chars + 1, step):
+    if cumulative==False:
+      img = np.copy(img_o)
+    drawCharMasks(img, ft, segmentations, start_idx=i, max_chars=step+1)
+    drawCharBoxes(img, segmentations, start_idx=i, max_boxes=step+1)
+    name = "{}/box_{}-{}.jpg".format(output_dir, i, i + step)
     cv2.imwrite(name, img)
 
 if __name__ == "__main__":
@@ -100,7 +111,7 @@ if __name__ == "__main__":
 
   settings = settings("config.json")
   output_dir = settings['debug_dir']
-  ft = FASTex(edgeThreshold=10, nlevels=-1, minCompSize = 4)
+  ft = FASTex(edgeThreshold=13, nlevels=-1, minCompSize = 4)
 
   # Read image as numpy array
   img = cv2.imread(args.image, 0) # load image in grayscale, i.e. 1 channel
@@ -111,7 +122,7 @@ if __name__ == "__main__":
   # Elem in rows:: [bbox.x, bbox.y, bbox.width, bbox.height, keyPoint.pt.x, keyPoint.pt.y, octave, ?, duplicate, quality, [keypointsIds]]
   segmentations = ft.getCharSegmentations(img, output_dir, 'base')
 
-  viz_location(img3, ft, segmentations, cumulative=False)
+  viz_location(img3, ft, segmentations, cumulative=True, max_chars=2000)
   # drawCharMasks(img3, ft, segmentations, 100)
   # drawCharBoxes(img3, segmentations, 100)
   # cv2.imwrite(output_dir + "/mask.jpg", img3)
